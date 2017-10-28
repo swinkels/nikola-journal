66e81f1b21c507d688027c6a02ca05ba07cd1caf
Pieter Swinkels <swinkels.pieter@yahoo.com>
2017-10-28 21:36:44 +0200

    Support content in org-mode format
    This commit adds the Nikola plugin to support org-mode content and configures
    its use in the config file.

--- conf.py
+++ conf.py
@@ -184,12 +184,14 @@ THEME_COLOR = '#5670d4'
 POSTS = (
     ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.md", "posts", "post.tmpl"),
+    ("posts/*.org", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
     ("posts/*.html", "posts", "post.tmpl"),
 )
 PAGES = (
     ("pages/*.rst", "pages", "page.tmpl"),
     ("pages/*.md", "pages", "page.tmpl"),
+    ("pages/*.org", "pages", "page.tmpl"),
     ("pages/*.txt", "pages", "page.tmpl"),
     ("pages/*.html", "pages", "page.tmpl"),
 )
@@ -263,6 +265,7 @@ TIMEZONE = "Europe/Amsterdam"
 # 'markdown' is Markdown
 # 'html' assumes the file is HTML and just copies it
 COMPILERS = {
+    "orgmode": ('.org',),
     "rest": ('.rst', '.txt'),
     "markdown": ('.md', '.mdown', '.markdown'),
     "textile": ('.textile',),
